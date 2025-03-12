0f4501afc309700c953ec9f9cc25d935ba66bf0b
sShi1ro <muhammad.awparif.hakimi@gmail.com>
2025-03-12 16:08:53 +0800

    Character Movement Animation

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
     void Update(){
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(x, y);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        //transform.position += new Vector3(x, y);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
