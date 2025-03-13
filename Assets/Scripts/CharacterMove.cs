efa89b7db0b44581586aaa83ae22eeb96861ffec
sShi1ro <muhammad.awparif.hakimi@gmail.com>
2025-03-13 15:18:25 +0800

    アニメーション後

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
+    private Vector2 lastDirection = Vector2.down;
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
-
     public Animator animator;
-
     Vector2 movement;
     void Update(){
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        //transform.position += new Vector3(x, y);
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.magnitude > 0){
+            lastDirection = movement.normalized;
+        }
+        animator.SetFloat("LastHorizontal", lastDirection.x);
+        animator.SetFloat("LastVertical", lastDirection.y);
     }
 
     void FixedUpdate(){
